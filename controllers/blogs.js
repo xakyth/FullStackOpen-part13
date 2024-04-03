21e0afaded3fc743fd18ef5d91273fad469b5f0f
xakyth <xakyth@gmail.com>
2024-04-03 07:10:59 +0300

    ex. 13.10 - creation of a blog linked to logged in user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 const { Blog } = require('../models');
+const { JWT_SECRET } = require('../util/config');
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), JWT_SECRET);
+    } catch {
+      return res.status(401).json({ error: 'token invalid' });
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' });
+  }
+  next();
+};
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
 });
 
-router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body);
+router.post('/', tokenExtractor, async (req, res) => {
+  const blog = await Blog.create({ ...req.body, userId: req.decodedToken.id });
   res.json(blog);
 });
 
