6f44b8641aa4d26ecbf85a4e51b6d7fffb7e3602
xakyth <xakyth@gmail.com>
2024-04-05 06:54:03 +0300

    ex. 13.16 - added GET /api/authors which returns info about author, number of likes, number of articles

---
+++ controllers/authors.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const { Blog } = require('../models');
+const { sequelize } = require('../models/blog');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+      [sequelize.fn('COUNT', sequelize.col('title')), 'articles'],
+    ],
+    group: 'author',
+    order: [['likes', 'DESC']],
+  });
+  res.json(blogs);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const { connectToDatabase } = require('./util/db');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const authorRouter = require('./controllers/authors');
 
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/authors', authorRouter);
 
 const errorHandler = (error, req, res, next) => {
   console.log(error);
