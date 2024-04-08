c5dc9bfbf10a1ccc83a0df7dd111db6eab75a9ca
xakyth <xakyth@gmail.com>
2024-04-08 07:14:51 +0300

    ex. 13.17 - migrations setup

---
+++ migrations/20240408_00_init_blogs_and_users.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      author: {
+        type: DataTypes.TEXT,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+    });
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          len: [3, 255],
+          isEmail: true,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs');
+    await queryInterface.dropTable('users');
+  },
+};
--- util/db.js
+++ util/db.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require('sequelize');
 const { DATABASE_URL } = require('./config');
+const { SequelizeStorage, Umzug } = require('umzug');
 
 const sequelize = new Sequelize(DATABASE_URL);
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log('Connected to the db');
   } catch (error) {
     console.log("Couldn't connect to db", error);
@@ -14,4 +16,34 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { sequelize, connectToDatabase };
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug({
+    logger: console,
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    context: sequelize.getQueryInterface(),
+    migrations: {
+      glob: 'migrations/*.js',
+    },
+  });
+  await migrator.down();
+};
+
+module.exports = { sequelize, connectToDatabase, rollbackMigration };
