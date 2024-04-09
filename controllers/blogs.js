7a3585011fbb240bbd52758e497c3ad819881cce
xakyth <xakyth@gmail.com>
2024-04-09 19:17:40 +0300

    tokenExtractor moved to util/middleware file

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,7 @@
 const router = require('express').Router();
-const jwt = require('jsonwebtoken');
 const { Blog, User } = require('../models');
-const { JWT_SECRET } = require('../util/config');
 const { Op } = require('sequelize');
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), JWT_SECRET);
-    } catch {
-      return res.status(401).json({ error: 'token invalid' });
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' });
-  }
-  next();
-};
+const { tokenExtractor } = require('../util/middleware');
 
 router.get('/', async (req, res) => {
   const search = req.query.search;
---
+++ util/middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('./config');
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), JWT_SECRET);
+    } catch {
+      return res.status(401).json({ error: 'token invalid' });
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' });
+  }
+  next();
+};
+
+module.exports = {
+  tokenExtractor,
+};
