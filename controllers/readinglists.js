22025b910d3d63b28be9e2800df7156f60db2cd4
xakyth <xakyth@gmail.com>
2024-04-09 19:26:47 +0300

    ex 13.22 - added PUT /api/readinglists/:id to update read state of the blog

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { ReadingList, Blog, User } = require('../models');
+const { tokenExtractor } = require('../util/middleware');
 
 router.post('/', async (req, res) => {
   const { blogId, userId } = req.body;
@@ -14,6 +15,24 @@ router.post('/', async (req, res) => {
 
   const createdEntry = await ReadingList.create({ userId, blogId });
   res.json(createdEntry);
+});
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const { id } = req.params;
+  let readingEntry = await ReadingList.findByPk(id);
+  if (!readingEntry) return res.status(404).end();
+
+  if (req.decodedToken.id !== readingEntry.userId)
+    return res.status(400).json({ error: 'no permission' });
+
+  const { read } = req.body;
+  if (!read)
+    return res.status(400).json({ error: 'read param should be specified' });
+
+  readingEntry.read = read;
+  readingEntry = await readingEntry.save();
+
+  res.json(readingEntry);
 });
 
 module.exports = router;
